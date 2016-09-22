1c07e19d7ba7482118f4188c2424b8929a4dbac2
Victor <alqace@gmail.com>
2016-09-22 12:25:54 -0300

    Modificacion a estructura de datos
    - Las constantes pasan a estar en mayusculas.
    - Dejo los nombres de las constantes como aparecen en la guia.
    - Cambio el orden de aparicion de las estructuras, siendo las mas pequeñas las primeras y las mas grandes al final.

--- tp1netflix.pas
+++ tp1netflix.pas
@@ -1,64 +1,65 @@
 program Netflix;
 uses crt;
 Const
-    maxserie=4;{predeterminado por la catedra ya que son cuatro series las que se piden}
-    maxtempxserie=10;{Valor elejido por el equipo comparando esta cantidad en diferentes sitios}
-    maxepixtemp=25;{Valor elejido comparando en distintos sitios cual usual es el numero de temporadas}
-    maxusuarios=60000;{predeterminado por el equipo}
-    maxvisualxusuario=1000;{predeterminado por el equipo}
+    MAX_CANTIDAD_SERIES = 4;{predeterminado por la catedra ya que son cuatro series las que se piden}
+    MAX_TEMPORADAS_POR_SERIE = 10;{Valor elejido por el equipo comparando esta cantidad en diferentes sitios}
+    MAX_EPISODIOS_POR_TEMPORADA = 25;{Valor elejido comparando en distintos sitios cual usual es el numero de temporadas}
+    MAX_USUARIOS = 60000;{predeterminado por el equipo}
+    MAX_VISUALIZACIONES_POR_USUARIO = 1000;{predeterminado por el equipo}
 
 Type
-    tserie=1..maxserie;
-    ttemp=1..maxtempxserie;
-    tepixtemp=1..maxepixtemp;
-    tusuario=0..maxusuarios;
-    tvisualizacion=0..maxvisualxusuario;
-
-    tregserie=record
-        nombre:string[71];
-        descripcion:string[140];
-        canttemp:byte;
-        temporadas:tvtemp;
-        end;
+    {Estructura de datos de las series}
+    trVideo = record 
+                titulo : string[72];
+                descripcion : string[234];
+                duracionEnSegundos : longint;
+                visualizaciones : longint
+                end;
     
-    tvecserie=array[tserie]of tregserie;
-
-    tregtemp= record
-        anioemision:string[4];
-        cantepixtemp:byte;
-        videos:tvvideos;
-        end;
+    tvVideo = array[1..MAX_EPISODIOS_POR_TEMPORADA] of trVideo;         
     
-    tvectemp=array[ttemp]of tregtemp;
-
-    tregvideos=record
-        titulo:string[22];
-        descripcion:string[234];
-        duracion:longint;
-        visualizacion:longint;
-        end;
+    trTemp = record
+                anioDeEmision : string[4];
+                cantEpiDeTemp : byte;
+                vVideos : tvVideos;
+                
+    tvTemp = array[1..MAX_TEMPORADAS_POR_SERIE] of trTemp;
     
-    tvecvideos=array[tepixtemp]of tregvideos;
-
-    tregvisualizacion=record
-        posicion:longint;
-        numerotemp:byte;
-        numeroepi:byte;
-        cantvisual:byte;
-        end;
+    trSerie = record
+                nombre : string[71];
+                descripcion : string[140];
+                cantTemp : byte;
+                vTemp : tvTemp;
+                
+    tvSerie = array[1..MAX_CANTIDAD_SERIES] of trSerie;
     
-    tvecvisual=array[tvisualizacion]of tregvisualizacion;
-
-    tregusuarios=record
-        nomusuario:string[8];
-        visualizacion:tvecvisual;
-        end;
     
-    tvecusuario=array[tusuario]of tregusuarios;
+    {Estructura de datos del usuario}
+    trVisualizacion = record 
+                posicionSerieEnArregloSerie : longint;
+                numTempEnLaSerie : byte;
+                numEpiDeLaTemp : byte;
+                cantVisualizaciones : byte;
+                
+    tvVisualizacion = array[1..MAX_VISUALIZACIONES_POR_USUARIO] of trVisualizacion;
+    
+    trUsuario = record 
+                nombreUsuario : string[8];
+                vVisualizacion : tvVisualizacion
+    
+    tvUsuario = array[1..MAX_USUARIOS] of trUsuario;
+    
+    
+    {Estructura de datos del vector auxiliar}
+    tvTop = array[1..CANT_TOP] of posicion;
 
-procedure inicializar(var vecserie:tvecserie;var vectemp:tvectemp;var vecvideos:tvecvideos;var vecusuario:tvecusuarios;var vecvisual:tvecvisual);
+ 
+
+procedure inicializar(var vSerie : tvSerie; var vtemp : tvTemp; var vVideo : tvVideo;
+                    var vUsuario : tvUsuario; var vVisualizacion : tvVisualizacion);
 var
 i:integer;
+
 begin
 
      for i:=1 to maxserie do 
@@ -73,51 +74,7 @@ begin
            vecvisual[i]:=0;
 end;
 
-Procedure cargarserie(var vecserie:tvecserie;var vectemp:tvectemp;var vecvideos:tvecvideos);
-var i:tserie;
-Begin
 
-    for i:=1 to maxserie do
-        Begin
-        writeln('Ingrese el nombre de la serie');
-        readln(vecserie[i].nombre);
-        writeln('Ingrese la descripcion de la serie');
-        readln(vecserie[i].descripcion);
-        writeln('Ingrese la cantidad de temporadas');
-        readln(vecserie[i].canttemp);
-        vecserie[i].temporadas:=cargartemporadas(vectemp,vecvideos,vecserie[i].canttemp);
-        end;      
-end;
-
-procedure cargartemporadas(var vectemp:tvectemp;var vecvideos:tvecvideos;ML:byte);
-var
- i,ML:byte;
-
-begin
-     for i:=1 to Ml do
-         Begin
-             writeln('Ingrese el año de emision de la temporada');
-             readln(vectemp[i].anioemision);
-             writeln('Ingrese la cantidad de episodios de la temporada');
-             readln(vectemp[i].cantepixtemp);
-             vectemp[i].videos:=cargarvideos(vecvideos,vectemp[i].cantepixtemp);
-         end;
-
-end;
-
-procedure cargarvideos(var vecvideos:tvecvideos; ML:byte);
-var
-i,ML:byte
-begin
-     for i:=1 to ML do
-         writeln('Ingrese el titulo del video');
-         readln(vecvideos[i].titulo);
-         writeln('Ingrese la descripcion del video');
-         readln(vecvideos[i].descripcion);
-         writeln('Ingrese la duracion del video en minutos');
-         readln(vecvideos[i].duracion);
-         vecvideo[i].visualizaciones:=generarvisualizaciones();
-end;
 
 procedure cargar_usuario(var vecusuario:tvecusuario);
 var
@@ -129,9 +86,6 @@ begin
             validarnombre(nom);
             validar 
             
-           
-
-
 end;
 
 {Declaracion de variables Globales}
